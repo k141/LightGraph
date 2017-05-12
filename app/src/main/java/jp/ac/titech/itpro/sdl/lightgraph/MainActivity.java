f2daf84115348749d3eeac5e50f5005af4294725
koji <ishii4itsp@gmail.com>
2017-05-12 17:04:37 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/lightgraph/MainActivity.java
@@ -0,0 +1,110 @@
+package jp.ac.titech.itpro.sdl.lightgraph;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private final static String TAG = "MainActivity";
+
+    private TextView rateView, accuracyView;
+    private GraphView lightView;
+
+    private SensorManager sensorMgr;
+    private Sensor lightSensor;
+
+    private final static long GRAPH_REFRESH_WAIT_MS = 20;
+
+    private GraphRefreshThread th = null;
+    private Handler handler;
+
+    private float vx;
+    private float rate;
+    private int accuracy;
+    private long prevts;
+
+    private final static float alpha = 0.75F;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
+        setContentView(R.layout.activity_main);
+
+        rateView = (TextView) findViewById(R.id.rate_view);
+        accuracyView = (TextView) findViewById(R.id.accuracy_view);
+        lightView = (GraphView) findViewById(R.id.x_view);
+
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lightSensor = sensorMgr.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if (lightSensor == null) {
+            Toast.makeText(this, getString(R.string.toast_no_sensor_error),
+                    Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        handler = new Handler();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+        sensorMgr.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        th = new GraphRefreshThread();
+        th.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+        th = null;
+        sensorMgr.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        vx = alpha * vx + (1 - alpha) * event.values[0];
+        rate = ((float) (event.timestamp - prevts)) / (1000 * 1000);
+        prevts = event.timestamp;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.i(TAG, "onAccuracyChanged: ");
+        this.accuracy = accuracy;
+    }
+
+    private class GraphRefreshThread extends Thread {
+        public void run() {
+            try {
+                while (th != null) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            rateView.setText(String.format(Locale.getDefault(), "%f", rate));
+                            accuracyView.setText(String.format(Locale.getDefault(), "%d", accuracy));
+                            lightView.addData(vx, true);
+                        }
+                    });
+                    Thread.sleep(GRAPH_REFRESH_WAIT_MS);
+                }
+            }
+            catch (InterruptedException e) {
+                Log.e(TAG, e.toString());
+                th = null;
+            }
+        }
+    }
+}
